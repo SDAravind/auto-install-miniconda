4ef1456686c51d61b7f607b65597675dee0245b5
Aravind <72403396+SDAravind@users.noreply.github.com>
2021-03-13 05:28:00 +0530

    Update venv_mc.ps1

--- install/update/venv_mc.ps1
+++ install/update/venv_mc.ps1
@@ -1,11 +1,11 @@
 # ############# create virtual env #############
 # $Env:_CONDA_ENV = "${home}\Miniconda3\Scripts\conda.exe"
 # $Env:_CONDA_ACT = "${home}\Miniconda3\condabin\conda.bat"
-Write-Output "Creating virtual environment py36 and installing required modules, please wait..."
-Invoke-Expression "conda create -n py36 python=3.6 ipykernel xlwings openpyxl xlrd xlwt xlsxwriter pandas scikit-learn pyspark seaborn -y"
+Write-Output "Creating virtual environment py38 and installing required modules, please wait..."
+Invoke-Expression "conda create -n py38 python=3.8 ipykernel xlwings openpyxl xlrd xlwt xlsxwriter pandas scikit-learn pyspark seaborn -y"
 
-Write-Output "Activating virtual environment 'py36'..."
-Invoke-Expression "conda activate py36"
+Write-Output "Activating virtual environment 'py38'..."
+Invoke-Expression "conda activate py38"
 
 Write-Output "Installing xlwings addin for excel..."
 Invoke-Expression "xlwings addin install"
